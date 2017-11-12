150440005d405272aeb6cbbbdcce57d5ac197d3b
Anna <anna@hydra.local>
2017-11-12 17:49:41 -0500

    Check in function

--- moringa_main_app/views.py
+++ moringa_main_app/views.py
@@ -1,17 +1,30 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.http import HttpResponse
+from moringa_main_app.models import Attendance
 import datetime
 
 # developer admin page
 def index(request):
-    context = "some context here"
-    return render(request, 'templates/student_view/index.html', context)
+    return render(request, 'student_view/index.html', None)
 
 # login
 def login(request):
-    context = "some context here"
-    return render(request, 'templates/student_view/index.html', context)
+    return render(request, 'student_view/index.html', None)
+
+# check in
+def check_in(request):
+    status = "on time"
+    if True: #condition for checking time
+        status = "tardy"
+    if request.method == 'POST':
+        if not request.POST.get('excuse'):
+            return render(request, 'student_view/check_in.html', {'student_status':status, 'error':True})
+        query = Attendance(userId=request.user, tardy=True, absent=False, excuse=request.POST.get('excuse'))
+        query.save()
+        # redirect to 'congrats, you've submitted' page
+    return render(request, 'student_view/check_in.html', {'student_status':status})
+        
 
 # VIEWS FOR STUDENT_VIEW
 class HomePageView(TemplateView):
